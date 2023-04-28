45a625c7ca86b6ff0f166a8e727d212f9d4d95c6
rafalradomski <rafal.radomski@hertz.com>
2023-04-28 09:18:13 +0100

    fix jest unit tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,23 @@
 import React from "react"
 import { screen } from "@testing-library/react"
-import { render } from "./test-utils"
+import '@testing-library/jest-dom';
+import { render } from "./test-utils/test-utils"
 import { App } from "./App"
+import {
+  ChakraProvider,
+  theme,
+} from "@chakra-ui/react"
+
+global.matchMedia = global.matchMedia || function () {
+  return {
+    matches: false,
+    addListener: jest.fn(),
+    removeListener: jest.fn(),
+  };
+};
 
 test("renders learn react link", () => {
-  render(<App />)
+  render(<ChakraProvider theme={theme}><App /></ChakraProvider>)
   const linkElement = screen.getByText(/learn chakra/i)
   expect(linkElement).toBeInTheDocument()
 })
